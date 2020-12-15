beb90930994f46f1b1b642202b0ba843bdb5b53f
Ruben <ruben.integrales@gmail.com>
2020-12-14 21:29:15 -0300

    inserting models

--- isnotagame/stage1/admin.py
+++ isnotagame/stage1/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Category, Questions, Stages
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "password", "email")
 
 admin.site.register(User, UserAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "categories")
+
+admin.site.register(Category, CategoryAdmin)
+
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ("id", "categories", "question", "answer1", "answer2", "answer3", "answer4", "correctanswerchar", "correctanswerint")
+
+admin.site.register(Questions, QuestionsAdmin)
+
+class StagesAdmin(admin.ModelAdmin):
+    list_display = ("id", "stage", "description")
+
+admin.site.register(Stages, StagesAdmin)
 
--- isnotagame/stage1/models.py
+++ isnotagame/stage1/models.py
@@ -3,5 +3,36 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    accomplishment = models.CharField(null=True, max_length=64)
+    date = models.DateField(null=True, blank=True)
+    time = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.time}"
+
+class Category(models.Model):
+    categories = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.categories}"
+
+class Questions(models.Model):
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    question = models.CharField(max_length=200)
+    answer1 = models.CharField(max_length=64)
+    answer2 = models.CharField(max_length=64)
+    answer3 = models.CharField(max_length=64)
+    answer4 = models.CharField(max_length=64)
+    correctanswerchar =  models.CharField(max_length=64)
+    correctanswerint =  models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.pk}"
+
+class Stages(models.Model):
+    stage = models.IntegerField(null=True, blank=True)
+    description = models.CharField(max_length=500)
+    userstat = models.ManyToManyField(User, blank=True, related_name="stages")
+
+    def __str__(self):
+        return f"{self.stage}"
